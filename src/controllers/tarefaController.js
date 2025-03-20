3182b30cade43e7ee3cc208924e48e77c59af11f
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-03-20 15:46:55 -0300

    feat: create delete method

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -48,12 +48,21 @@ class TarefaController {
     }
   };
 
-  delete = ({ params: { id } }, res) => {
-    const sucesso = tarefaModel.delete(id);
-    if (!sucesso) {
-      return res.status(404).json({ erro: "Tarefa não encontrada" });
+  delete = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const sucesso = tarefaModel.delete(Number(id));
+
+      if (!sucesso) {
+        return res.status(404).json({ erro: "Tarefa não encontrada" });
+      }
+
+      res.status(200).send({ message: "Tarefa deletada com sucesso!" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Erro ao excluir tarefa"});
     }
-    res.status(204).send();
   };
 }
 export default new TarefaController();
